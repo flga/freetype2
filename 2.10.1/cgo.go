72e2289a004917244cffca8fbbdc8018d9d00ef5
flga <flga@users.noreply.github.com>
2019-07-19 18:40:40 +0100

    chore(2.10.1): Add OSX binaries

--- 2.10.1/cgo.go
+++ 2.10.1/cgo.go
@@ -13,6 +13,16 @@ package freetype2
 // #cgo linux,amd64,static,harfbuzz LDFLAGS: -L${SRCDIR}/linux_amd64/lib -lfreetypehb -lharfbuzz -lfreetypehb -lbz2 -lpng16 -lz -lm
 // #cgo linux,amd64,static,harfbuzz,subset LDFLAGS: -L${SRCDIR}/linux_amd64/lib -lfreetypehb -lharfbuzz -lharfbuzz-subset -lbz2 -lpng16 -lz -lm
 //
+// #cgo darwin,386,static CFLAGS: -I${SRCDIR}/darwin_386/include/freetype2 -Werror -Wall -Wextra -Wno-unused-parameter
+// #cgo darwin,386,static,!harfbuzz LDFLAGS: -L${SRCDIR}/darwin_386/lib -lfreetype -lbz2 -lpng16 -lz -lm
+// #cgo darwin,386,static,harfbuzz LDFLAGS: -L${SRCDIR}/darwin_386/lib -lfreetypehb -lharfbuzz -lfreetypehb -lbz2 -lpng16 -lz -lm
+// #cgo darwin,386,static,harfbuzz,subset LDFLAGS: -L${SRCDIR}/darwin_386/lib -lfreetypehb -lharfbuzz -lharfbuzz-subset -lbz2 -lpng16 -lz -lm
+//
+// #cgo darwin,amd64,static CFLAGS: -I${SRCDIR}/darwin_amd64/include/freetype2 -Werror -Wall -Wextra -Wno-unused-parameter
+// #cgo darwin,amd64,static,!harfbuzz LDFLAGS: -L${SRCDIR}/darwin_amd64/lib -lfreetype -lbz2 -lpng16 -lz -lm
+// #cgo darwin,amd64,static,harfbuzz LDFLAGS: -L${SRCDIR}/darwin_amd64/lib -lfreetypehb -lharfbuzz -lfreetypehb -lbz2 -lpng16 -lz -lm
+// #cgo darwin,amd64,static,harfbuzz,subset LDFLAGS: -L${SRCDIR}/darwin_amd64/lib -lfreetypehb -lharfbuzz -lharfbuzz-subset -lbz2 -lpng16 -lz -lm
+//
 // #include <stdlib.h>
 // #include <ft2build.h>
 // #include FT_FREETYPE_H
