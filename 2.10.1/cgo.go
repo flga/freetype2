06a1be8acb988bd635660bb12cef001eeee4cc25
fleandro <3987005+flga@users.noreply.github.com>
2019-07-19 05:42:52 +0100

    fix(ci): Move to xenial dist (#3)
    Issue was caused by https://github.com/golang/go/issues/31293#issuecomment-480512935

--- 2.10.1/cgo.go
+++ 2.10.1/cgo.go
@@ -4,14 +4,14 @@ package freetype2
 // #cgo !static,!windows pkg-config: freetype2
 //
 // #cgo linux,386,static CFLAGS: -I${SRCDIR}/linux_386/include/freetype2 -Werror -Wall -Wextra -Wno-unused-parameter
-// #cgo linux,386,static,!harfbuzz LDFLAGS: -L${SRCDIR}/linux_386/lib -lfreetype -lm
-// #cgo linux,386,static,harfbuzz LDFLAGS: -L${SRCDIR}/linux_386/lib -lfreetypehb -lm
-// #cgo linux,386,static,harfbuzz,subset LDFLAGS: -L${SRCDIR}/linux_386/lib -lfreetypehb-subset -lm
+// #cgo linux,386,static,!harfbuzz LDFLAGS: -L${SRCDIR}/linux_386/lib -lfreetype -lbz2 -lpng16 -lz -lm
+// #cgo linux,386,static,harfbuzz LDFLAGS: -L${SRCDIR}/linux_386/lib -lfreetypehb -lharfbuzz -lfreetypehb -lbz2 -lpng16 -lz -lm
+// #cgo linux,386,static,harfbuzz,subset LDFLAGS: -L${SRCDIR}/linux_386/lib -lfreetypehb -lharfbuzz -lharfbuzz-subset -lbz2 -lpng16 -lz -lm
 //
 // #cgo linux,amd64,static CFLAGS: -I${SRCDIR}/linux_amd64/include/freetype2 -Werror -Wall -Wextra -Wno-unused-parameter
-// #cgo linux,amd64,static,!harfbuzz LDFLAGS: -L${SRCDIR}/linux_amd64/lib -lfreetype -lm
-// #cgo linux,amd64,static,harfbuzz LDFLAGS: -L${SRCDIR}/linux_amd64/lib -lfreetypehb -lm
-// #cgo linux,amd64,static,harfbuzz,subset LDFLAGS: -L${SRCDIR}/linux_amd64/lib -lfreetypehb-subset -lm
+// #cgo linux,amd64,static,!harfbuzz LDFLAGS: -L${SRCDIR}/linux_amd64/lib -lfreetype -lbz2 -lpng16 -lz -lm
+// #cgo linux,amd64,static,harfbuzz LDFLAGS: -L${SRCDIR}/linux_amd64/lib -lfreetypehb -lharfbuzz -lfreetypehb -lbz2 -lpng16 -lz -lm
+// #cgo linux,amd64,static,harfbuzz,subset LDFLAGS: -L${SRCDIR}/linux_amd64/lib -lfreetypehb -lharfbuzz -lharfbuzz-subset -lbz2 -lpng16 -lz -lm
 //
 // #include <stdlib.h>
 // #include <ft2build.h>
--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM golang:1.12-stretch
 
 COPY . /go/src/github.com/flga/freetype2
-RUN go test -v -tags static github.com/flga/freetype2/2.10.1
-RUN go test -v -tags 'static harfbuzz' github.com/flga/freetype2/2.10.1
-RUN go test -v -tags 'static harfbuzz subset' github.com/flga/freetype2/2.10.1
+RUN CGO_ENABLED=1 GOARCH=amd64 go test -tags static -ldflags "-linkmode external -extldflags -static" github.com/flga/freetype2/2.10.1
+RUN CGO_ENABLED=1 GOARCH=amd64 go test -tags 'static harfbuzz' -ldflags "-linkmode external -extldflags -static" github.com/flga/freetype2/2.10.1
+RUN CGO_ENABLED=1 GOARCH=amd64 go test -tags 'static harfbuzz subset' -ldflags "-linkmode external -extldflags -static" github.com/flga/freetype2/2.10.1
