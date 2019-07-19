7aa5788d78bb33bb26018c796f7385f27c96363b
flga <flga@users.noreply.github.com>
2019-07-19 17:58:25 +0100

    chore(ci): Move tests to shell script

--- test.sh
+++ test.sh
@@ -2,10 +2,12 @@
 set -e
 
 OS=$(go env GOHOSTOS)
-if [[ "$OS" == "linux" ]]; then
-    goLDFlags='-ldflags "-linkmode external -extldflags -static"'
+if [ "$OS" == "linux" ]; then
+    goLDFlags=(-ldflags "-linkmode external -extldflags -static")
 fi
 
-CGO_ENABLED=1 GOARCH=$(ARCH) go test -v -tags 'static' $(goLDFlags) -coverage github.com/flga/freetype2/$(VERSION)
-CGO_ENABLED=1 GOARCH=$(ARCH) go test -v -tags 'static harfbuzz' $(goLDFlags) -coverage github.com/flga/freetype2/$(VERSION)
-CGO_ENABLED=1 GOARCH=$(ARCH) go test -v -tags 'static harfbuzz subset' $(goLDFlags) -coverage github.com/flga/freetype2/$(VERSION)
+cd $VERSION
+
+CGO_ENABLED=1 GOARCH=$ARCH go test -tags 'static' "${goLDFlags[@]}" -coverprofile ./...
+CGO_ENABLED=1 GOARCH=$ARCH go test -tags 'static harfbuzz' "${goLDFlags[@]}" -coverprofile ./...
+CGO_ENABLED=1 GOARCH=$ARCH go test -tags 'static harfbuzz subset' "${goLDFlags[@]}" -coverprofile ./...
